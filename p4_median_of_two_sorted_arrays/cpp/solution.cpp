648339bd9c5f7e8eb542cade891d847db3deec73
wsix <wsix0114@gmail.com>
2017-04-03 21:12:08 +0800

    Update solution for p4

--- p4_median_of_two_sorted_arrays/cpp/solution.cpp
+++ p4_median_of_two_sorted_arrays/cpp/solution.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int getkth(int s[], int m, int l[], int n, int k){
+    int getkth(vector<int>& nums1, vector<int>& nums2, int k){
+        int m = nums1.size();
+        int n = nums2.size();
         // let m <= n
         if (m > n)
-            return getkth(l, n, s, m, k);
+            return getkth(nums2, nums1, k);
         if (m == 0)
-            return l[k - 1];
+            return nums2[k - 1];
         if (k == 1)
-            return min(s[0], l[0]);
+            return min(nums1[0], nums2[0]);
 
         int i = min(m, k / 2), j = min(n, k / 2);
-        if (s[i - 1] > l[j - 1])
-            return getkth(s, m, l + j, n - j, k - j);
-        else
-            return getkth(s + i, m - i, l, n, k - i);
+        if (nums1[i - 1] > nums2[j - 1]) {
+            vector<int> rst_nums2(nums2.begin()+j, nums2.end());
+            return getkth(nums1, rst_nums2, k - j);
+        } else {
+            vector<int> rst_nums1(nums1.begin()+i, nums1.end());
+            return getkth(rst_nums1, nums2, k - i);
+        }
         return 0;
     }
 
-    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        size_t m = nums1.size();
+        size_t n = nums2.size();
         int l = (m + n + 1) >> 1;
         int r = (m + n + 2) >> 1;
-        return (getkth(A, m ,B, n, l) + getkth(A, m, B, n, r)) / 2.0;
+        return (getkth(nums1, nums2, l) + getkth(nums1, nums2, r)) / 2.0;
     }
 };
